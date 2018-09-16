4dc812456e3938006762bf94c7104cf67192e3ef
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-09-16 19:15:42 +0700

    use array on todo items

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,24 @@
 import React from 'react';
 
 class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        'Study React',
+        'Shopping',
+        'Play games',
+        'Sleep'
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <div><h2>Todos</h2></div>
-        <div className="py-2"><button className="btn btn-danger btn-sm mr-2">DEL</button>Study React</div>
-        <div className="py-2"><button className="btn btn-danger btn-sm mr-2">DEL</button>Shopping</div>
-        <div className="py-2"><button className="btn btn-danger btn-sm mr-2">DEL</button>Play games </div>
-        <div className="py-2"><button className="btn btn-danger btn-sm mr-2">DEL</button>Sleep</div>
+        {
+          this.state.items.map((item, index) => <div className="py-2" key={index}><button className="btn btn-danger btn-sm mr-2">DEL</button>{item}</div>)
+        }
       </div>
     )
   }
