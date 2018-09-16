7879131449c1d1e1cc1533e8abcbc51fb97fa414
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-09-16 19:32:00 +0700

    add click handler

--- src/TodoList.js
+++ src/TodoList.js
@@ -4,12 +4,22 @@ class TodoList extends React.Component {
   constructor(props) {
     super(props);
   }
+  handleClick = index => {
+    console.log(index);
+  }
   render() {
     return (
       <div>
         <div><h2>Todos</h2></div>
         {
-          this.props.items.map((item, index) => <div className="py-2" key={index}><button className="btn btn-danger btn-sm mr-2">DEL</button>{item}</div>)
+          this.props.items.map((item, index) =>
+            <div className="py-2" key={index}>
+              <button
+                className="btn btn-danger btn-sm mr-2"
+                onClick={() => this.handleClick(index)}
+              >DEL</button>
+              {item}
+            </div>)
         }
       </div>
     )
