8f55f9612e6a2d36853f98537e67843eaa4b68b8
Chatpong Suteesuksataporn <chatpong@progaming.co.th>
2018-09-16 19:19:06 +0700

    move item state to App and pass down as props

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        'Study React',
+        'Shopping',
+        'Play games',
+        'Sleep'
+      ]
+    }
+  }
   render() {
     return (
       <div className="container">
         <TodoForm title="Add new item" />
-        <TodoList />
+        <TodoList items={this.state.items} />
       </div>
     );
   }
--- src/TodoList.js
+++ src/TodoList.js
@@ -3,21 +3,13 @@ import React from 'react';
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      items: [
-        'Study React',
-        'Shopping',
-        'Play games',
-        'Sleep'
-      ]
-    }
   }
   render() {
     return (
       <div>
         <div><h2>Todos</h2></div>
         {
-          this.state.items.map((item, index) => <div className="py-2" key={index}><button className="btn btn-danger btn-sm mr-2">DEL</button>{item}</div>)
+          this.props.items.map((item, index) => <div className="py-2" key={index}><button className="btn btn-danger btn-sm mr-2">DEL</button>{item}</div>)
         }
       </div>
     )
